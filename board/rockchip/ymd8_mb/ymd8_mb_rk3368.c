b295a4aafaf5deacffb7165a5a4534b15649b7c7
WeiHao Li <cn.liweihao@gmail.com>
2025-08-25 11:00:03 +0800

    add of board operation
    Signed-off-by: WeiHao Li <cn.liweihao@gmail.com>

--- board/rockchip/ymd8_mb/ymd8_mb_rk3368.c
+++ board/rockchip/ymd8_mb/ymd8_mb_rk3368.c
@@ -3,13 +3,17 @@
  * Authors: Weihao Li <cn.liweihao@gmail.com>
  */
 
+#include <dm.h>
 #include <init.h>
 #include <syscon.h>
+#include <backlight.h>
 #include <asm/global_data.h>
+#include <dm/uclass-internal.h>
 #include <asm/arch-rockchip/hardware.h>
 #include <asm/arch-rockchip/clock.h>
 #include <asm/arch-rockchip/grf_rk3368.h>
 #include <asm/arch-rockchip/cru_rk3368.h>
+#include <asm/arch-rockchip/vop_rk3288.h>
 
 int board_early_init_r(void)
 {
@@ -19,8 +23,21 @@ int board_early_init_r(void)
 #if defined(CONFIG_OF_BOARD_SETUP)
 int ft_board_setup(void *blob, struct bd_info *bd)
 {
-	if (IS_ENABLED(CONFIG_FDT_SIMPLEFB))
-		fdt_simplefb_enable_and_mem_rsv(blob);
+	struct udevice *dev;
+	void *regs;
+	struct rk3288_vop *vop;
+
+	uclass_find_first_device(UCLASS_VIDEO, &dev);
+	if (dev) {
+		regs = dev_read_addr_ptr(dev);
+		vop = regs;
+		/* VOP enter standby mode. */
+		clrsetbits_le32(&vop->sys_ctrl, BIT(22), BIT(22));
+	}
+
+	uclass_find_first_device(UCLASS_PANEL_BACKLIGHT, &dev);
+	if (dev)
+		backlight_set_brightness(dev, 0);
 
 	return 0;
 }
--- include/configs/ymd8_mb.h
+++ include/configs/ymd8_mb.h
@@ -10,11 +10,12 @@
 
 #undef CFG_EXTRA_ENV_SETTINGS
 #define CFG_EXTRA_ENV_SETTINGS \
+	"partitions=" PARTS_DEFAULT \
 	"fdtfile=" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
 	ENV_MEM_LAYOUT_SETTINGS \
 	"boot_targets=" BOOT_TARGETS "\0" \
 	"splashsource=mmc_fs\0" \
-	"splashdevpart=0:6\0" \
+	"splashdevpart=0:4\0" \
 	"splashpos=m,m\0" \
 	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 
